df7cab2f1d502acb7215f200b3f7d4aef2af5c16
Maxim Turbal <tor968@yahoo.com>
2020-08-19 22:41:10 +0300

    Update task_5.js

--- js/task_5.js
+++ js/task_5.js
@@ -1,14 +1,43 @@
 class Car {
   // Write code under this line
-  getSpecs() {}
-  constructor() {}
-  get price() {}
-  set price(value) {}
-  turnOn() {}
-  turnOff() {}
-  accelerate(value) {}
-  decelerate(value) {}
-  drive(hours) {}
+
+  getSpecs() {
+    return (this.car = car);
+  }
+  constructor(maxSpeed, speed = 0, isOn = Boolean, distance = 0, price) {
+    (this.maxSpeed = maxSpeed),
+      (this.speed = speed),
+      (this.isOn = isOn),
+      (this.distance = distance),
+      (this.price = price);
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    return (this.isOn = true);
+  }
+  turnOff() {
+    return (this.isOn = false);
+  }
+  accelerate(value) {
+    if (this.speed < this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+  decelerate(value) {
+    if (this.speed > 0) {
+      this.speed -= value;
+    }
+  }
+  drive(hours) {
+    if ((this.isOn = true)) {
+      this.distance = hours / this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
@@ -17,7 +46,7 @@ mustang.accelerate(50);
 mustang.drive(2);
 
 // console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+// ('maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000');
 
 mustang.decelerate(20);
 mustang.drive(1);
